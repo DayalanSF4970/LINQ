a0733b014e772c78b5b21b029c052202858c8907
DayalanSF4970 <dayalan.loganathan@syncfusion.com>
2025-02-17 21:33:39 +0530

    Linq_exercises

---
+++ Linq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program{
+    public static void Main(string[] args)
+    {
+        List<string> cities =["ABU DHABI","AMSTERDAM","ROME","MADURAI","LONDON","NEW DELHI","MUMBAI","NAIROBI"];
+        System.Console.WriteLine("Start character");
+        char startChar=char.ToUpper(Console.ReadLine()[0]);
+        System.Console.WriteLine("End character");
+        char endChar=char.ToUpper(Console.ReadLine()[0]);
+        var resultCities=cities.Where(c=>c.StartsWith(startChar.ToString(),StringComparison.OrdinalIgnoreCase) && c.EndsWith(endChar.ToString(),StringComparison.OrdinalIgnoreCase)).ToList();
+        if(resultCities.Count != 0)
+        {
+            System.Console.WriteLine($"Cities found with startChar-{startChar} and endChar-{endChar}: {string.Join(",",resultCities)}");
+        }
+        var sortedCities=cities.OrderBy(city=>city.Length).ThenBy(city=>city).ToList();
+        if(sortedCities.Count != 0)
+        {
+            System.Console.WriteLine($"Cities in ascending order: {string.Join(",",sortedCities)}");
+        }
+    }
+}
